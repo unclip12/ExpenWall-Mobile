3d0212be0933eb2953cc2ba5e886e00b2724e37a
unclip12 <unclip-kite-stooge@duck.com>
2026-02-04 14:57:29 +0530

    Integrate ThemeProvider and ThemedBackground in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'screens/splash_screen.dart';
 import 'screens/home_screen_v2.dart';
-import 'services/theme_service.dart';
-import 'widgets/animated_gradient_background.dart';
+import 'providers/theme_provider.dart';
+import 'widgets/themed_background.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,69 +22,31 @@ void main() async {
   // Enable edge-to-edge
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  static _MyAppState? of(BuildContext context) {
-    return context.findAncestorStateOfType<_MyAppState>();
-  }
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _themeService = ThemeService();
-  AppThemeType _currentTheme = AppThemeType.midnightPurple;
-  bool _isDarkMode = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadThemePreferences();
-  }
-
-  Future<void> _loadThemePreferences() async {
-    final theme = await _themeService.getTheme();
-    final darkMode = await _themeService.isDarkMode();
-    setState(() {
-      _currentTheme = theme;
-      _isDarkMode = darkMode;
-    });
-  }
-
-  Future<void> toggleDarkMode(bool isDark) async {
-    setState(() => _isDarkMode = isDark);
-    await _themeService.setDarkMode(isDark);
-  }
-
-  Future<void> changeTheme(AppThemeType theme) async {
-    setState(() => _currentTheme = theme);
-    await _themeService.setTheme(theme);
-  }
 
   @override
   Widget build(BuildContext context) {
-    final themeData = _themeService.getThemeData(_currentTheme, _isDarkMode);
-    final gradientColors = _themeService.getGradientColors(_currentTheme, _isDarkMode);
-
-    return MaterialApp(
-      title: 'ExpenWall',
-      debugShowCheckedModeBanner: false,
-      theme: themeData,
-      home: AnimatedGradientBackground(
-        colors: gradientColors,
-        child: const SplashScreen(
-          nextScreen: HomeScreenV2(),
-        ),
-      ),
-      builder: (context, child) {
-        return AnimatedGradientBackground(
-          colors: gradientColors,
-          child: child ?? const SizedBox(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'ExpenWall',
+          debugShowCheckedModeBanner: false,
+          theme: themeProvider.currentTheme,
+          home: ThemedBackground(
+            config: themeProvider.backgroundConfig,
+            child: const SplashScreen(
+              nextScreen: HomeScreenV2(),
+            ),
+          ),
         );
       },
     );
